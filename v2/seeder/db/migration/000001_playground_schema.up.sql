f3312258536c6df7b2d3a793a2bc0b1180941e38
Rakai Seto <mamangrakai@gmail.com>
2022-06-01 23:22:19 +0700

    add proto

--- v2/seeder/db/migration/000001_playground_schema.up.sql
+++ v2/seeder/db/migration/000001_playground_schema.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE public.product (
 GRANT UPDATE, TRUNCATE, REFERENCES, INSERT, DELETE, TRIGGER, SELECT ON TABLE public.product TO productschema;
 
 CREATE TABLE public.user (
-   id SERIAL primary key,
+   id SERIAL PRIMARY KEY,
    uname VARCHAR,
    email VARCHAR,
    password VARCHAR,
@@ -16,7 +16,7 @@ CREATE TABLE public.user (
 GRANT UPDATE, TRUNCATE, REFERENCES, INSERT, DELETE, TRIGGER, SELECT ON TABLE public.user TO userschema;
 
 CREATE TABLE public.order (
-   id SERIAL primary key,
+   id SERIAL PRIMARY KEY DEFAULT,
    userid integer not null,
    productid integer not null,
    quantity integer not null,
